69b92fe205aa2b473a5c381e0557b44c82aca7bc
FabioIngenito <fabio.ingenito@hotmail.com>
2024-03-28 11:49:02 -0300

    arquivo de escopo

---
+++ escopo.js
@@ -0,0 +1,19 @@
+if (true) {
+    var doguinho = "Poodle";
+
+    console.log(`Raça do doguinho: ${doguinho}`);
+    console.log(`String do doguinho: ${typeof(doguinho)}`);
+}
+
+doguinho = "Caramelo"
+console.log(`Tipo do doguinho: ${doguinho}`);
+console.log(`String do doguinho: ${typeof(doguinho)}`);
+
+if (1 > 0) {
+    let gatinho = 'Persa'
+    console.log(`Raça do gato: ${gatinho}`)
+}
+
+gatinho = "Rajado";
+//var implícito
+console.log(gatinho)
