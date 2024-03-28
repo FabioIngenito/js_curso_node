ddb7955d018deadd3e13cc17f2a4ed02167602c2
FabioIngenito <fabio.ingenito@hotmail.com>
2024-03-28 11:33:00 -0300

    Frescura minha.

---
+++ index.tsx
@@ -0,0 +1,12 @@
+
+// https://bun.sh/
+
+const server = Bun.serve({
+    port: 3000,
+    fetch(request) {
+      return new Response("Welcome to Bun!");
+    },
+  });
+  
+  console.log(`Listening on localhost:${server.port}`);
+  
