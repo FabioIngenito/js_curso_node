fb5f8290b1f90215a3a9986bc310d59166acb322
FabioIngenito <fabio.ingenito@hotmail.com>
2024-03-28 11:32:37 -0300

    chore: execício com variáveis

---
+++ variaveis.js
@@ -0,0 +1,23 @@
+var pet = "Cachorro";
+var gatinho = 'Pet';
+// String - testo puro
+const idadePet = 5
+
+// TEMPLATE STRING
+console.log(`String var pet: ${typeof(pet)}`);
+console.log(`Valor pet: ${pet}`);
+console.log("--------------------------")
+
+pet = "Mini Pig"
+console.log(`String var pet: ${typeof(pet)}`);
+console.log(`Valor pet: ${pet}`);
+
+// Já é "var"
+doguinho = "Caramelo"
+
+
+let = professor = "Thiago"
+
+var vacina = true;
+
+
