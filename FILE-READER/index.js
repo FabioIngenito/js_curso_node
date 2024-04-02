d0257cb62b2cb798119d79037fc078c1ee9b2090
FabioIngenito <fabio.ingenito@hotmail.com>
2024-04-02 11:43:38 -0300

    chore: commit initial - aula03 - 02/04/2024

---
+++ FILE-READER/index.js
@@ -0,0 +1,74 @@
+import chalk from 'chalk';
+import fs from 'fs';
+
+function extrairLinks(texto) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map((captura) => ({
+    [captura[0]]: captura[1],
+  }));
+
+  return resultados;
+}
+
+// OBS.: Se tiver uma "extensão" ligada (exemplo: "Origamid Next"), influencia a cor do Chalk. SOMENTE DESLIGUE A EXTENSÃO, não precisa desinstalar.
+// console.log(chalk.green('Olá, mundo!'));
+// console.log(chalk.bgMagenta('Olá, mundo!'));
+
+function tratarErro(erro) {
+  console.log(erro);
+  throw new Error(chalk.bgYellowBright.red(erro.code, '-> NÃO é um arquivo.'));
+}
+
+function pegarArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8';
+  // SEM tratamento de erro:
+  //fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+    if (erro) tratarErro(erro);
+
+    console.log(chalk.green(texto));
+  });
+}
+
+function pegarArquivoAssincrona(caminhoDoArquivo) {
+  const encoding = 'utf-8';
+
+  fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(chalk.green(texto)))
+    .catch(tratarErro);
+}
+
+//pegarArquivo("./arquivos/texto.md");
+
+// FORÇAR UM ERRO:
+// pegarArquivo("./arquivos/");
+
+// ------------------------
+// ASSÍNCRONA:
+//pegarArquivoAssincrona('./arquivos/texto.md');
+
+// FORÇAR UM ERRO:
+// pegarArquivoAssincrona("./arquivos/");
+
+async function asyncPegarArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+
+    //console.log(chalk.green(texto));
+
+    // teste = extrairLinks(texto);
+    // console.log(chalk.red(teste));
+
+    console.log(extrairLinks(texto));
+  } catch (error) {
+    tratarErro(error);
+  }
+}
+
+asyncPegarArquivo('./arquivos/texto.md');
+
+// FORÇAR UM ERRO:
+// asyncPegarArquivo('./arquivos/');
